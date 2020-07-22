009142d6b07973224ad1c0fd80fa2bc4ff932750
Juan Duzac <jduzac@fi.uba.ar>
2020-07-22 19:55:58 -0300

    Agregue Test PreguntaVyF Asigna puntajes a los jugadores

--- src/main/test/edu/fiuba/algo3/modelo/PreguntaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PreguntaTest.java
@@ -26,7 +26,7 @@ public class PreguntaTest {
     }
 
     @Test
-    public void test02PreguntaVerdaderoFalsoTieneRespuestaCorrecta(){
+    public void test02PreguntaVerdaderoFalsoClasicoTieneRespuestaCorrecta(){
         //Arrange
         String textoPregunta = "La UBA fue fundada en el año 1821";
         String[] opciones = {"True","False"};
@@ -39,5 +39,37 @@ public class PreguntaTest {
 
         //Assert
         assertEquals(respuesta, respuestaCorrecta);
+    }
+
+    @Test
+    public void test03PreguntaVerdaderoFalsoClasicoRecibeRespuestasYAsignaPunajeALosJugadores() {
+        //Arrange
+        Jugador jugador1 = new Jugador("Jose");
+        Jugador jugador2 = new Jugador("Lucas");
+        Jugador[] jugadores = {jugador1, jugador2};
+
+        String textoPregunta = "La UBA fue fundada en el año 1821";
+        String[] opciones = {"True", "False"};
+
+        String[] opcionJugador1 = {"True"};
+        RespuestaSimple respuestaJugador1 = new RespuestaSimple(opcionJugador1);
+
+        String[] opcionJugador2 = {"False"};
+        RespuestaSimple respuestaJugador2 = new RespuestaSimple(opcionJugador2);
+
+        RespuestaSimple[] respuestasJugadores = {respuestaJugador1, respuestaJugador2};
+
+        String[] opcionCorrecta = {"True"};
+        RespuestaSimple respuestaCorrecta = new RespuestaSimple(opcionCorrecta);
+
+        Pregunta unaPregunta = new Pregunta(textoPregunta, opciones, respuestaCorrecta);
+
+        //Act
+        unaPregunta.asignarPuntajes(respuestasJugadores, jugadores);
+
+        //Assert
+        assertEquals(jugador1.getPuntaje(), 1);
+        assertEquals(jugador2.getPuntaje(), 0);
+
     }
 }
