00b39e284d24fcb3cd5aef0c4ee255889ea3cf00
Juan Duzac <jduzac@fi.uba.ar>
2020-08-06 12:48:34 -0300

    elimino un print que habia quedado para debuguear

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/Puntaje.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/Puntaje.java
@@ -1,15 +1,12 @@
 package edu.fiuba.algo3.modelo.preguntas;
 
-import edu.fiuba.algo3.modelo.Jugador;
 import edu.fiuba.algo3.modelo.Respuesta.Respuesta;
-import edu.fiuba.algo3.modelo.Booster;
 
 public abstract class Puntaje {
 
 
     public void asignarPuntaje(Respuesta respuesta){
         int puntajeASumar = calcularPuntaje(respuesta);
-        System.out.print(puntajeASumar);
         respuesta.sumarPuntajeAJugador(puntajeASumar);
     }
 
