8711e56b129090375b6ce1ba17436242b5991a9b
fdelaplata <fdelaplata@fi.uba.ar>
2020-08-12 02:36:44 -0300

    Agrego los tipos de pregunta que faltaban en CreadorDePreguntas

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/CreadorDePreguntas.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/CreadorDePreguntas.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.modelo.preguntas;
 
 import edu.fiuba.algo3.modelo.Excepciones.FalloAlLeerRespuestaArchivoError;
 import edu.fiuba.algo3.modelo.Excepciones.FalloLaInicializacionDelArchivoError;
+import edu.fiuba.algo3.modelo.Grupo;
 import edu.fiuba.algo3.modelo.Valor;
 import edu.fiuba.algo3.modelo.opciones.Opcion;
 
@@ -10,9 +11,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 
 public class CreadorDePreguntas {
 
@@ -20,17 +19,84 @@ public class CreadorDePreguntas {
         ArrayList<Pregunta> preguntas = new ArrayList<>();
 
         preguntas.addAll(cargarPreguntasVoF());
-//      preguntas.addAll(cargarPreguntasOrdered());
-//      preguntas.addAll(cargarPreguntasGroup());
+        preguntas.addAll(cargarPreguntasOrdered());
+        preguntas.addAll(cargarPreguntasGroup());
+
+        Collections.shuffle(preguntas);
 
         return preguntas;
+    }
+
+    private static ArrayList<Pregunta> cargarPreguntasGroup() {
+        /*  FORMATO ARCHIVO
+            TextoPregunta ; Opciones
+
+            TIPO PUNTAJE
+            Por consigna las group son clásicas
+
+            FORMATO OPCIONES
+            Grupo1 | Op1 | Op2 | Op3 ~ Grupo2 ; Op4 ; Op5
+            Precondición por consigna: Son desde 2 hasta 5 opciones.
+
+            EJEMPLOS
+            A que grupo pertenecen las siguientes opciones;Frutas|Tomate|Sandia~Banana|Verduras|Lechuga|Berenjena
+         */
+
+        FileReader archivo;
+        BufferedReader lector;
+
+        ArrayList<Pregunta> preguntas = new ArrayList<>();
+        try {
+            archivo = new FileReader("preguntasGrupal.txt");
+
+            if(archivo.ready()) {
+                lector = new BufferedReader(archivo);
+                String lineaEntera ;
+                String[] separadoPorComas;
+
+                while ((lineaEntera = lector.readLine()) != null) {
+                    separadoPorComas = lineaEntera.split(";");
+
+                    ArrayList<Opcion> opciones = parsearOpcionesGrupal(separadoPorComas[1]);
+                    Puntaje puntaje = new PuntajeClasico();
+
+                    Pregunta pregunta = new Pregunta(separadoPorComas[1], opciones, puntaje);
+
+                    preguntas.add(pregunta);
+                }
+                archivo.close();
+            }
+        } catch (IOException | FalloAlLeerRespuestaArchivoError e) {
+            throw new FalloLaInicializacionDelArchivoError();
+        }
+        return preguntas;
+    }
+
+    private static ArrayList<Opcion> parsearOpcionesGrupal(String textoOpciones) {
+        ArrayList<Opcion> opciones = new ArrayList<>();
+        Opcion opcion;
+
+        String[] arrayTextoOpciones = textoOpciones.split("~");
+        Grupo grupo;
+        int i = 0;
+        for (String textoOpcion: arrayTextoOpciones){
+            i++;
+            String[] arrayTextoOpcion = textoOpcion.split("|");
+            grupo = new Grupo(arrayTextoOpcion[0], i);
+            for (int j = 1; j < arrayTextoOpcion.length; j++){
+                opcion = new Opcion(arrayTextoOpcion[j], new Valor(grupo));
+                opciones.add(opcion);
+            }
+        }
+
+        return opciones;
     }
 
     private static ArrayList<Opcion> parsearOpcionesVoF(String textoOpciones, String textoOpcionesCorrectas) {
         ArrayList<Opcion> opciones = new ArrayList<>();
         Opcion opcion;
 
-        List<String> arrayTextoOpciones= Arrays.asList(textoOpciones.split("-"));
+        String[] arrayTextoOpciones = textoOpciones.split("-");
 
         for (String textoOpcion: arrayTextoOpciones){
             opcion = new Opcion(textoOpcion, new Valor(textoOpcionesCorrectas.contains(textoOpcion)) );
@@ -52,10 +118,10 @@ public class CreadorDePreguntas {
             conPenalidad;En qué año fue fundada la UBA?;1876-1816-1821-1913;1821
          */
 
-        FileReader archivo = null;
-        BufferedReader lector = null;
+        FileReader archivo;
+        BufferedReader lector;
 
-        ArrayList<Pregunta> preguntas = new ArrayList<Pregunta>();
+        ArrayList<Pregunta> preguntas = new ArrayList<>();
         try {
             archivo = new FileReader("preguntasEleccionSimple.txt");
 
@@ -69,18 +135,20 @@ public class CreadorDePreguntas {
 
                     ArrayList<Opcion> opciones = parsearOpcionesVoF(separadoPorComas[2], separadoPorComas[3]);
                     Puntaje puntaje;
+
                     //Leo el TipoPuntaje
-                    if (separadoPorComas[0].equals("clasico")){
-                        puntaje = new PuntajeClasico();
-                    }
-                    else if (separadoPorComas[0].equals("parcial")){
-                        puntaje = new PuntajeParcial();
-                    }
-                    else if (separadoPorComas[0].equals("conPenalidad")){
-                        puntaje = new PuntajePenalidad();
-                    }
-                    else{
-                        throw new FalloAlLeerRespuestaArchivoError();
+                    switch (separadoPorComas[0]) {
+                        case "clasico":
+                            puntaje = new PuntajeClasico();
+                            break;
+                        case "parcial":
+                            puntaje = new PuntajeParcial();
+                            break;
+                        case "conPenalidad":
+                            puntaje = new PuntajePenalidad();
+                            break;
+                        default:
+                            throw new FalloAlLeerRespuestaArchivoError();
                     }
 
                     Pregunta pregunta = new Pregunta(separadoPorComas[1], opciones, puntaje);
@@ -95,7 +163,7 @@ public class CreadorDePreguntas {
         return preguntas;
     }
 
-    /*private static ArrayList<PreguntaVyF> cargarPreguntasOrdered(){*/
+    private static ArrayList<Pregunta> cargarPreguntasOrdered(){
         /*  FORMATO ARCHIVO
             TextoPregunta ; Opciones
 
@@ -111,10 +179,10 @@ public class CreadorDePreguntas {
             conPenalidad;En qué año fue fundada la UBA?;1876-1816-1821-1913;1821
          */
 
-        /*FileReader archivo = null;
-        BufferedReader lector = null;
+        FileReader archivo;
+        BufferedReader lector;
 
-        ArrayList<PreguntaVyF> preguntas = new ArrayList<PreguntaVyF>();
+        ArrayList<Pregunta> preguntas = new ArrayList<>();
         try {
             archivo = new FileReader("preguntasOrdered.txt");
 
@@ -126,10 +194,10 @@ public class CreadorDePreguntas {
                 while ((lineaEntera = lector.readLine()) != null) {
                     separadoPorComas = lineaEntera.split(";");
 
-                    ArrayList<OpcionVerdaderoFalso> opciones = parsearOpcionesOrdered(separadoPorComas[2]);
+                    ArrayList<Opcion> opciones = parsearOpcionesOrdered(separadoPorComas[1]);
                     Puntaje puntaje = new PuntajeClasico();
 
-                    PreguntaVyF pregunta = new PreguntaVyF(separadoPorComas[0], opciones, puntaje);
+                    Pregunta pregunta = new Pregunta(separadoPorComas[0], opciones, puntaje);
 
                     preguntas.add(pregunta);
                 }
@@ -141,8 +209,19 @@ public class CreadorDePreguntas {
         return preguntas;
     }
 
-    private static ArrayList<OpcionVerdaderoFalso> parsearOpcionesOrdered(String separadoPorComa) {
-        // TO DO
-        return new ArrayList<OpcionVerdaderoFalso>();
+    private static ArrayList<Opcion> parsearOpcionesOrdered(String textoOpciones) {
+        ArrayList<Opcion> opciones = new ArrayList<>();
+        Opcion opcion;
+
+        String[] arrayTextoOpciones = textoOpciones.split("-");
+
+        int i = 0;
+        for (String textoOpcion: arrayTextoOpciones){
+            i++;
+            opcion = new Opcion(textoOpcion, new Valor(i));
+            opciones.add(opcion);
+        }
+
+        return opciones;
     }
-*/}
+}
