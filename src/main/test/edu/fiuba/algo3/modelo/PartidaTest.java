2b53cc21dbac40cc5ee71e734dd37094358e6aef
Juan Duzac <jduzac@fi.uba.ar>
2020-08-20 15:39:45 -0300

    test para  excepciones NoHayTurno, NoHayRonda

--- src/main/test/edu/fiuba/algo3/modelo/PartidaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PartidaTest.java
@@ -2,9 +2,15 @@ package edu.fiuba.algo3.modelo;
 
 import edu.fiuba.algo3.modelo.excepciones.NoHaySiguienteRondaError;
 import edu.fiuba.algo3.modelo.excepciones.NoHaySiguienteTurnoError;
-import edu.fiuba.algo3.modelo.preguntas.CreadorDePreguntas;
-import edu.fiuba.algo3.modelo.preguntas.Pregunta;
+import edu.fiuba.algo3.modelo.preguntas.*;
+import edu.fiuba.algo3.modelo.preguntas.puntajes.Puntaje;
+import edu.fiuba.algo3.modelo.preguntas.puntajes.PuntajeClasico;
+import edu.fiuba.algo3.modelo.preguntas.puntajes.PuntajePenalidad;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -69,4 +75,78 @@ public class PartidaTest {
         assertEquals(2, numeroDeRonda);
         assertEquals( partida.getPreguntaActual(), preguntaActual);
     }
+
+
+
+    @Test
+    public void test04NoSePuedePasarDelUltimoTurno() throws NoHaySiguienteTurnoError {
+        //Arrange
+        Partida partida = new Partida();
+        partida.agregarJugador("Juan");
+        partida.agregarJugador("Pedro");
+        partida.inicializarPartida(CreadorDePreguntas.crearPreguntas());
+
+        //Act
+
+        partida.siguienteTurno();
+
+        //Assert
+        Assertions.assertThrows(NoHaySiguienteTurnoError.class, () -> {
+            partida.siguienteTurno();
+        });
+    }
+
+    @Test
+    public void test05NoSePuedePasarDelUltimaRonda() throws NoHaySiguienteRondaError {
+        //Arrange
+        Partida partida = new Partida();
+        partida.agregarJugador("Juan");
+        partida.agregarJugador("Pedro");
+
+        //Pregunta 1
+        Puntaje puntaje1 = new PuntajeClasico();
+        ArrayList<Opcion> opciones1 = new ArrayList<>();
+
+        Opcion opcionIncorrecta = new Opcion("falso", new Valor(false));
+        Opcion opcionCorrecta = new Opcion("verdadero", new Valor(true));
+
+        opciones1.add(opcionIncorrecta);
+        opciones1.add(opcionCorrecta);
+        String textoPregunta1 = "La UBA fue fundada en el año 1821";
+
+        Pregunta pregunta1VyFClasico = new Pregunta(textoPregunta1, opciones1, puntaje1, "dummyText");
+
+
+        //Pregunta 2
+        Puntaje puntaje2 = new PuntajePenalidad();
+
+        Opcion opcion1 = new Opcion("1", new Valor(false));
+        Opcion opcion2 = new Opcion("2", new Valor(true));
+        Opcion opcion3 = new Opcion("3", new Valor(true));
+        Opcion opcion4 = new Opcion("4", new Valor(false));
+        Opcion opcion5 = new Opcion("5", new Valor(true));
+        Opcion opcion6 = new Opcion("6", new Valor(false));
+
+        ArrayList<Opcion> opciones2 = new ArrayList<>(
+                Arrays.asList(opcion1, opcion2, opcion3, opcion4, opcion5, opcion6)
+        );
+
+        String textoPregunta2 = "¿Cuáles de los siguientes números son primos?";
+
+        Pregunta pregunta2MultiplePenalidad = new Pregunta(textoPregunta2, opciones2, puntaje2, "dummyText");
+
+
+        //Preguntas
+        ArrayList<Pregunta> preguntas = new ArrayList<>(Arrays.asList(pregunta1VyFClasico, pregunta2MultiplePenalidad));
+        partida.inicializarPartida(preguntas);
+
+        //Act
+        partida.siguienteRonda();
+
+        //Assert
+        Assertions.assertThrows(NoHaySiguienteRondaError.class, () -> {
+            partida.siguienteRonda();
+        });
+    }
+
 }
